117561d3a89f939c905d22539c448c304e8c1876
Biniunko <beninasefa@gmail.com>
2025-07-10 20:20:53 +0300

    updated for the pyhon sentence

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,15 @@
 
-function spell(c,cool){
+/*function spell(c,cool){
     console.log(c," is ",cool)
 }
 spell("c","cool")
 spell("c",)
 spell()
+*/
+const args = process.argv.slice(2);
+
+if (args[0] && args[1]) {
+  console.log(`${args[0]} is ${args[1]}`);
+} else if (args[0]) {
+  console.log(args[0]);
+}
