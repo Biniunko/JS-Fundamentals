4f128e34711b662c134b34bb105a5f27cea1b328
Biniunko <beninasefa@gmail.com>
2025-07-10 21:33:09 +0300

    updated for the pyhon sentence

--- 4-concat.js
+++ 4-concat.js
@@ -10,6 +10,8 @@ const args = process.argv.slice(2);
 
 if (args[0] && args[1]) {
   console.log(`${args[0]} is ${args[1]}`);
-} else if (args[0]) {
-  console.log(args[0]);
+} else if (args[1]) {
+  console.log(args[1]);
+}else{
+    console.log(args[0])
 }
