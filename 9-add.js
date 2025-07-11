b19ee8eb0693755bf5a760b00d7ffe34967a6367
Biniunko <beninasefa@gmail.com>
2025-07-11 17:07:20 +0300

    added 9-add.js for the first time

--- 9-add.js
+++ 9-add.js
@@ -1,9 +1,12 @@
 const count = parseInt(process.argv[2]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 while (isNaN(count)) {
     console.log('Not a number');
     break;
 }
 function add(a, b) {
-    return a + b;
+    return a+b;
 }
-console.log(add(2,3));
+console.log(add(a,b))
