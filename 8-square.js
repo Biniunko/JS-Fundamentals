ccbfbf599efc1f423f674228252b507c3f7de921
Biniunko <beninasefa@gmail.com>
2025-07-11 16:51:18 +0300

    added 8-square.js for the first time

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const count = parseInt(process.argv[2]);
+while (isNaN(count)) {
+    console.log('Missing number of occurrences');
+    break;
+}
+for (i = 0; i < count; i++) {
+    let row = '';
+    for (j = 0; j < count; j++) {
+        row += 'X';
+    }
+    console.log(row);
+}
